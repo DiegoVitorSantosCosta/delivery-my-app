c3e3c0bd0fae26d2820bed45105af9db4dbdb4bf
DiegoVitorSantosCosta <diegovitorsantoscosta2017@gmail.com>
2021-08-02 19:30:35 -0300

    ok

--- mysql.js
+++ mysql.js
@@ -7,11 +7,17 @@ var pool = mysql2.createPool({
     // "host": "172.17.0.2",
     // "port": 3306
 
-    "user":"ba56a182084032",
-    "password": "3f0fd637",
-    "database": "heroku_a85c027beac9687",
-    "host": "us-cdbr-east-04.cleardb.com",
-    "port": 3306
+      "user":process.env.MYSQL_USER,
+    "password": process.env.MYSQL_PASSWORD,
+    "database": process.env.MYSQL_DATABASE,
+    "host": process.env.MYSQL_HOST,
+    "port": process.env.MYSQL_PORT
+
+    // "user":"ba56a182084032",
+    // "password": "3f0fd637",
+    // "database": "heroku_a85c027beac9687",
+    // "host": "us-cdbr-east-04.cleardb.com",
+    // "port": 3306
     
 })
 
