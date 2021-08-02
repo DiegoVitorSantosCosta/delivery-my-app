5aa879e77fa1785ccc32e5335e5e99df94318c43
DiegoVitorSantosCosta <diegovitorsantoscosta2017@gmail.com>
2021-08-02 18:54:30 -0300

    ok

--- mysql.js
+++ mysql.js
@@ -1,10 +1,16 @@
 const mysql2 = require('mysql2');
 
 var pool = mysql2.createPool({
-    "user":"root",
-    "password": "diegovitor",
-    "database": "banco_api",
-    "host": "172.17.0.2",
+    // "user":"root",
+    // "password": "diegovitor",
+    // "database": "banco_api",
+    // "host": "172.17.0.2",
+    // "port": 3306
+
+    "user":"ba56a182084032",
+    "password": "3f0fd637",
+    "database": "heroku_a85c027beac9687",
+    "host": "us-cdbr-east-04.cleardb.com",
     "port": 3306
     
 })
