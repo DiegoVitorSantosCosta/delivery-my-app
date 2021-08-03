e71fd292ec57f4ca80a34f105286201a83ad0323
DiegoVitorSantosCosta <diegovitorsantoscosta2017@gmail.com>
2021-08-03 10:44:57 -0300

    ok

--- app.js
+++ app.js
@@ -16,9 +16,30 @@ app.use('/products',routesProducts);
 // app.use('/upload',routesUploads);
 
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
 
-app.post('/upload', upload.single('picture'),(req,res) =>{
+
+// Configuração de armazenamento
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+        // Extração da extensão do arquivo original:
+        const extensaoArquivo = file.originalname.split('.')[1];
+
+        // Cria um código randômico que será o nome do arquivo
+        const novoNomeArquivo = require('crypto')
+            .randomBytes(64)
+            .toString('hex');
+
+        // Indica o novo nome do arquivo:
+        cb(null, `${novoNomeArquivo}.${extensaoArquivo}`)
+    }
+});
+
+const upload = multer({ storage });
+
+app.post('/cadastro', upload.single('picture'),(req,res) =>{
     
     
     const {lastName,numbe,picture} = req.body;
