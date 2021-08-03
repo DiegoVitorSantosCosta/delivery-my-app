510fc731bce6cd0444958a302775438128190a86
DiegoVitorSantosCosta <diegovitorsantoscosta2017@gmail.com>
2021-08-03 05:38:36 -0300

    ok

--- server.js
+++ server.js
@@ -5,4 +5,9 @@ const server = http.createServer(app)
 server.listen(port, () => {
     console.log("App is running on port " + port);
 });
+
+app.use(function(req, res, next) {
+    var reqType = req.headers["x-forwarded-proto"];
+    reqType == 'https' ? next() : res.redirect("https://" + req.headers.host + req.url);
+});
 // server.listen(port)
