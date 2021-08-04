2d1a2ce3e2e9e2f29a7b143eedf6d8c8a574c48e
DiegoVitorSantosCosta <diegovitorsantoscosta2017@gmail.com>
2021-08-04 09:45:50 -0300

    ok

--- routes/uploads.js
+++ routes/uploads.js
@@ -8,10 +8,10 @@ const multer = require('multer');
 const storage = multer.diskStorage({
 
     destination: function (req,file,cb){
-        cb(null, 'uploads/')
+        cb(null, './uploads/')
     },
     filename: function (req,file,cb){
-        cb(null, file.mimetype)
+        cb(null, file.originalname)
     }
 })
 
