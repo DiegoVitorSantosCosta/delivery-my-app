26972ae5110c2d42a46e0e4d8545cbab1af8889b
DiegoVitorSantosCosta <diegovitorsantoscosta2017@gmail.com>
2021-08-04 10:43:45 -0300

    ok

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const http = require('http')
 const app = require('./app')
 const port = process.env.PORT || 8000;
-// const port = 3001;
 const server = http.createServer(app)
 server.listen(port, () => {
     console.log("App is running on port " + port);
 });
 
-server.listen(port)
+// server.listen(port)
