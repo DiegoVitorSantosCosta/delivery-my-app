2b0b0fb03c5a07dd81ef65f80b77712271c6f8a2
DiegoVitorSantosCosta <diegovitorsantoscosta2017@gmail.com>
2021-08-04 09:04:33 -0300

    ok

--- app.js
+++ app.js
@@ -15,29 +15,6 @@ app.use(express.json()) // entrada de json no body
 app.use('/products',routesProducts);
 app.use('/cadastro',routesUploads);
 
-// const multer = require('multer');
-
-
-// Configuração de armazenamento
-// const storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//         cb(null, 'uploads/')
-//     },
-//     filename: function (req, file, cb) {
-//         // Extração da extensão do arquivo original:
-//         const extensaoArquivo = file.originalname.split('.')[1];
-
-//         // Cria um código randômico que será o nome do arquivo
-//         const novoNomeArquivo = require('crypto')
-//             .randomBytes(64)
-//             .toString('hex');
-
-//         // Indica o novo nome do arquivo:
-//         cb(null, `${novoNomeArquivo}.${extensaoArquivo}`)
-//     }
-// });
-
-// const upload = multer({ storage });
 
 app.use((req,res,next)=>{
     const error = new Error('Não encontrado ...')
--- routes/uploads.js
+++ routes/uploads.js
@@ -4,25 +4,50 @@ const app = express();
 const router = express.Router();
 const mysql = require('../mysql').pool;
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
+
+
+
+// Configuração de armazenamento
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+        // Extração da extensão do arquivo original:
+        const extensaoArquivo = file.originalname.split('.')[1];
+
+        // Cria um código randômico que será o nome do arquivo
+        const novoNomeArquivo = require('crypto')
+            .randomBytes(64)
+            .toString('hex');
+
+        // Indica o novo nome do arquivo:
+        cb(null, `${novoNomeArquivo}.${extensaoArquivo}`)
+    }
+});
+
+
+
+
+const upload = multer({ storage: storage });
 
 router.post('/', upload.single('picture'),(req,res,next) =>{
     
     
     const { picture } = req.file;
     res.status(201).send({ result: req.file })
-    // mysql.getConnection((error,conn) => {
-    //     if(error) return res.status(500).send({ menssage: error });
+    mysql.getConnection((error,conn) => {
+        if(error) return res.status(500).send({ menssage: error });
 
-    //     conn.query(
-    //         (error,result,field) => {
-    //             conn.release();
-    //             if(error) return res.status(500).send( { menssage: error });
+        conn.query(
+            (error,result,field) => {
+                conn.release();
+                if(error) return res.status(500).send( { menssage: error });
 
             
-    //         }
-    //     )
-    // })
+            }
+        )
+    })
 })
 
 
