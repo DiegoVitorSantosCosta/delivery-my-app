d579fd2a565da1097e781088bb8be4cd18ae7ff8
DiegoVitorSantosCosta <diegovitorsantoscosta2017@gmail.com>
2021-08-04 21:36:40 -0300

    ok

--- app.js
+++ app.js
@@ -7,9 +7,15 @@ const routesUploads = require('./routes/uploads')
 // configurações de cors
 app.use((req,res,next)=>{
     // dendo permissão maxima aqui
-    res.header("Acces-Control-Allow-Origin","*")
+    res.header("Acces-Control-Allow-Origin","*"),
     
-    r
+    res.header("Acces-Control-Allow-Header",
+    "Origin,X-Requested-With,Content-type,Accept")
+
+    if(req.method === "OPTIONS"){
+        res.header("Acess-Control-Allow-Methods","PUT,POST,PATCH,GET,DELET")
+        return res.status(200).send({})
+    }
     next();
 })
 
