e0d5e56e0de9598218685ebc2e5f78e59d54fc2f
DiegoVitorSantosCosta <diegovitorsantoscosta2017@gmail.com>
2021-08-05 10:23:18 -0300

    ok

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ const routesUploads = require('./routes/uploads')
 
 // configurações de cors
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, X-Requested-With');
+    
 
     // intercept OPTIONS method
     if ('OPTIONS' == req.method) {
+        res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, X-Requested-With');
       res.send(200);
     }
     else {
