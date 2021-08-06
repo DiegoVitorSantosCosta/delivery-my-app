e0b46ac4fcb00f5d4e31c1305607ac8fcbb8775c
DiegoVitorSantosCosta <diegovitorsantoscosta2017@gmail.com>
2021-08-05 22:39:07 -0300

    ok

--- routes/login.js
+++ routes/login.js
@@ -32,21 +32,22 @@ router.post('/',(req,res,next) =>{
 
                     if(response){ 
 
-                        const token = jwt.sign({
-                            id_user: result[0].id,
-                            email: result[0].email
-                        },
+                        // const token = jwt.sign({
+                        //     id_user: result[0].id,
+                        //     email: result[0].email
+                        // },
 
-                        process.env.JWT_KEY,
-                        {
-                            expiresIn: "1h"
-                        });
+                        // process.env.JWT_KEY,
+                        // {
+                        //     expiresIn: "1h"
+                        // });
 
 
                         return res.status(201).send({ 
-                            menssage: 'login realizado com sucesso',
-                            token: token
-                         });}
+                            menssage: 'login realizado com sucesso'
+                            // token: token
+                         });
+                        }
 
                     return res.status(401).send({ menssage: 'senha errada' });
                    
