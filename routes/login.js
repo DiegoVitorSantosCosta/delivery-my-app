555e0535ea1c4c9c91461f7119a9ba189e4f57cc
DiegoVitorSantosCosta <diegovitorsantoscosta2017@gmail.com>
2021-08-05 22:19:50 -0300

    ok

--- routes/login.js
+++ routes/login.js
@@ -19,16 +19,16 @@ router.post('/',(req,res,next) =>{
 
             async (error,result,field) => {
                 conn.release();
-                
+
                 if(error) return res.status(500).send({ menssage: error });
 
                 if(result.length < 1) return   res.status(409).send({ menssage: 'email não existe' }) ;
 
                
-                 bcrypt.compare(req.body.password,result[0].password,(err,hash)=>{
+                 bcrypt.compare(req.body.password,result[0].password,(err,response)=>{
                     if(err) return res.status(401).send({ menssage: 'falha na autentificação do email' });
 
-                    if(result) return res.status(201).send({ menssage: 'login realizado com sucesso' });
+                    if(response) return res.status(201).send({ menssage: 'login realizado com sucesso' });
 
                     return res.status(401).send({ menssage: 'senha errada' });
                    
