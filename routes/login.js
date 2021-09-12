79dda71e4c24993d80a9b5d94c2d8bb42315a17f
DiegoVitorSantosCosta <diegovitorsantoscosta2017@gmail.com>
2021-09-12 12:05:35 -0300

    make it better

--- routes/login.js
+++ routes/login.js
@@ -1,20 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const cors = require('cors')
 const mysql = require('../mysql').pool;
 var jwt = require('jsonwebtoken');
 
-// deixa em hash a senha
 
-var corsOptions = {
-
-    origin: 'http://localhost:8100',
-
-    optionsSuccessStatus: 200 ,// some legacy browsers (IE11, various SmartTVs) choke on 204
-
-    
-  }
-router.post('/', cors(corsOptions),(req,res,next) =>{    
+router.post('/',(req,res,next) =>{    
 
     mysql.getConnection((error,conn) => {
 
