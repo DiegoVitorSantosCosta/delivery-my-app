5f52591c263fdca9c413c0b7de2efed195498673
DiegoVitorSantosCosta <diegovitorsantoscosta2017@gmail.com>
2021-09-12 11:51:13 -0300

    make it better

--- app.js
+++ app.js
@@ -9,13 +9,8 @@ const routerLogin = require('./routes/login');
 const cors = require('cors');
 
 // configurações de cors
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    app.use(cors());
-    next();
-    })
+app.use(cors())
+
 
 
 app.use('/uploads',express.static('uploads'));
